1d008819b3b724269b6c7f5fbe053f31a89a6605
codegoat23 <codesmrt@proton.me>
2026-05-30 15:39:50 -0700

    fix add testimonial

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,17 @@
 
-import WhoizCTA from "@/components/CTA";
+
 import Dynamicstyle from "@/components/Dynamicstyle";
-import WhoizHeroBlock from "@/components/Example";
-import FAQ from "@/components/faq";
+
 import FeatureButton from "@/components/FeatureButton";
-import AboutWhoiz from "@/components/Features";
+
 import WhoizFooter from "@/components/footer";
 import Hero from "@/components/hero";
-import HowItWorks from "@/components/howitworks";
+
 import PortfolioSection from "@/components/PortfolioSection";
-import Products from "@/components/products";
-import WhoizSteps from "@/components/steps";
-import Testimonials from "@/components/testimonials";
-import Typograph from "@/components/typograph";
-import WhoizFeatures from "@/components/whoizfeatures";
-import Steps from "@/components/whoizsteps";
-import Widgets from "@/components/widgets";
-import Image from "next/image";
+
+
+
+
 
 export default function Home() {
   return (
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,13 +1,10 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { ArrowUpRight, CirclePlay } from "lucide-react";
-import { BackgroundPattern } from "@/components/background-pattern";
+
 import Link from "next/link";
 import GlassNavbar from "./navbar";
-import SlideScale from "./carousel-11";
-import WhoizBioInput from "./linkinput";
-import FloatingAvatars from "./Heroavatar";
-import Cardcoursel from "./cardcoursel";
+
 import Hero2 from "./hero2";
 
 export default function Hero() {
